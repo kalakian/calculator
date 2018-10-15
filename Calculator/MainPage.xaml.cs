6c993a9eceba54c90f331c19ab1f061bfc87943d
Derek Stobbs <dstobbs@googlemail.com>
2018-10-15 15:08:09 +0100

    Digits
    * Digits can be added to the display, up to a max length
    * Decimal point can be added to display, if none already there
    * Display can be cleared

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -1,17 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Calculator
 {
   public partial class MainPage : ContentPage
   {
+    const int MAX_DIGITS = 10;
+
+    private string displayText;
+
     public MainPage()
     {
       InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+      base.OnAppearing();
+
+      ClearDisplay();
+      RefreshDisplay();
+    }
+
+    private void ClearDisplay()
+    {
+      displayText = "0";
+    }
+
+    private void RefreshDisplay()
+    {
+      displayLabel.Text = displayText;
+      if(!displayText.Contains('.'))
+      {
+        displayLabel.Text += '.';
+      }
+    }
+
+    private void DigitButton_Clicked(object sender, EventArgs e)
+    {
+      if(displayText.Count(char.IsDigit) == MAX_DIGITS)
+        return;
+
+      if(displayText == "0")
+      {
+        displayText = (sender as Button).Text;
+      }
+      else
+      {
+        displayText += (sender as Button).Text;
+      }
+      RefreshDisplay();
+    }
+
+    private void ClearButton_Clicked(object sender, EventArgs e)
+    {
+      ClearDisplay();
+      RefreshDisplay();
+    }
+
+    private void DecimalPointButton_Clicked(object sender, EventArgs e)
+    {
+      if(!displayText.Contains('.'))
+      {
+        displayText += '.';
+      }
     }
   }
 }
